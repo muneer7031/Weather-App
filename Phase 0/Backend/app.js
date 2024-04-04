2b599a77957d2d3dff4e20565249f5d6f2065b30
Muneer7031 <muneer7031@gmail.com>
2024-04-03 22:36:44 -0400

    frontend folder created

--- Phase 0/Backend/app.js
+++ Phase 0/Backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const hbs= require('hbs')
 const app = express();
 const port = 8080;
 const getAllData = require('./scripts/allData');
+const getViewData = require('./scripts/viewdata');
 
-// Set up template engine
-// app.set('view engine', 'hbs');
+// Set up handlebars engine
+app.set('view engine', 'hbs');
+app.set('views',__dirname + '/views');
 
 app.get('/', (req, res) => {
     res.send('Welcome to my server!');
@@ -12,26 +16,25 @@ app.get('/', (req, res) => {
 
 // Route to fetch all data
 app.get('/alldata', (req, res) => {
-  getAllData((err, data) => {
-      if (err) {
-          res.status(500).send('Internal Server Error');
-          return;
-      }
-      res.json(data); // Send JSON response
-  });
+    getAllData((err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.json(data); // Send JSON response
+    });
 });
 
 // Route to fetch all data and render using template engine
 app.get('/viewdata', (req, res) => {
-  getViewData((err, data) => {
-      if (err) {
-          res.status(500).send('Internal Server Error');
-          return;
-      }
-      res.render('index',{countries: data}); // Send JSON response
-  });
+    getViewData((err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.render('viewdata', { countries: data }); // Pass 'countries' object to the template
+    });
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- Phase 0/Backend/scripts/viewdata.js
+++ Phase 0/Backend/scripts/viewdata.js
@@ -5,24 +5,23 @@ function getViewData(callback) {
     fs.readFile('./data/country.json', 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading JSON file:', err);
-            callback(err, null);
-            return;
+            return callback(err, null);
         }
 
         try {
             // Parse the JSON data
             const jsonData = JSON.parse(data);
-            jsonData=jsonData.country.map(item => ({
-                name:item.name,
-                capital:item["@capital"],
+            // Extract required data from JSON
+            const countries = jsonData.country.map(item => ({
+                name: item.name,
+                capital: item["@capital"],
                 memberships: item["@memberships"],
                 gdp_total: item.gdp_total,
                 inflation: item.inflation,
                 unemployment: item.unemployment,
                 government: item.government
             }));
-            
-
+            callback(null, countries);
         } catch (error) {
             console.error('Error parsing JSON data:', error);
             callback(error, null);
